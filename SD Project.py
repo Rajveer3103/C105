79e19aa0845e478b349dc80abdca426e47736f17
Rajveer3103 <63953244+Rajveer3103@users.noreply.github.com>
2022-07-15 17:47:01 +0530

    Add files via upload

---
+++ SD Project.py
@@ -0,0 +1,21 @@
+import math
+import csv
+data=[60,61,65,63,98,99,90,95,91,96]
+def mean(data):
+    n=len(data)
+    total=0
+    for x in data:
+        total+=int(x)
+    mean=total/n
+    return mean
+square_list=[]
+for number in data:
+    a=int(number)-mean(data)
+    a=a**2
+    square_list.append(a)
+sum=0
+for i in square_list:
+    sum=sum+i
+result=sum/(len(data)-1)
+std_deviation=math.sqrt(result)
+print(std_deviation)
